61ea088d5a792af9dc6ece4a0b97e379ba7b648a
Matthew Dibble <mdibble@rogers.com>
2020-08-02 22:56:48 -0400

    Reformatted skills page

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -9,60 +9,40 @@ function Skills() {
             <h1>Skills</h1>
             <SkillGroup type='Creative'>
                 <p>Image Manipulation</p>
-                <span>Photoshop</span>
-                <span>Lightroom</span>
-                <span>GIMP</span>
-                <span>Illustrator</span>
+                <span>Photoshop, Lightroom, GIMP, Illustrator</span>
                 <br />
                 <p>Video Editing</p>
-                <span>Premiere Pro</span>
-                <span>Final Cut</span>
+                <span>Premiere Pro, Final Cut</span>
                 <br />
                 <p>Compositing</p>
-                <span>After Effects</span>
-                <span>Nuke</span>
+                <span>After Effects, Nuke</span>
                 <br />
                 <p>3D</p>
-                <span>Maya</span>
-                <span>Cinema 4D</span>
-                <span>Houdini</span>
-                <span>Arnold</span>
+                <span>Maya, Cinema 4D, Houdini, Arnold</span>
             </SkillGroup>
             <SkillGroup type='Web'>
                 <p>Languages</p>
-                <span>HTML</span>
-                <span>CSS</span>
-                <span>JavaScript</span>
+                <span>HTML, CSS, JavaScript</span>
                 <br />
                 <p>Technologies</p>
-                <span>React</span>
-                <span>Node</span>
-                <span>MongoDB</span>
-                <span>Express</span>
-                <span>Azure</span>
+                <span>React, Node.js, MongoDB, Express, Azure</span>
             </SkillGroup>
             <SkillGroup type='Languages'>
                 <p>General-purpose</p>
-                <span>C</span>
-                <span>C++</span>
-                <span>Java</span>
-                <span>Python</span>
+                <span>C, C++, Java, Python</span>
                 <br />
                 <p>Scripting</p>
-                <span>VBA</span>
-                <span>Google Apps Script</span>
+                <span>VBA, Google Apps Script</span>
                 <br />
                 <p>Other</p>
                 <span>SQL</span>
             </SkillGroup>
             <SkillGroup type='Eager to Learn'>
                 <p>Hardware</p>
-                <span>CHIP-8</span>
-                <span>Emulation</span>
+                <span>CHIP-8, Emulation</span>
                 <br />
                 <p>Languages</p>
-                <span>Rust</span>
-                <span>Go</span>
+                <span>Rust, Go</span>
                 <br />
                 <p>Frameworks</p>
                 <span>TensorFlow</span>
